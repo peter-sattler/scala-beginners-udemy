32a400565d5929ec979e0165c92469d009ca62f5
Pete Sattler <peter@sattler22.net>
2022-01-17 13:19:16 -0500

    Updated section 2 - call by name vs call by value

--- src/main/scala/net/sattler22/lectures/part1basics/CBNvsCBV.scala
+++ src/main/scala/net/sattler22/lectures/part1basics/CBNvsCBV.scala
@@ -3,15 +3,17 @@ package net.sattler22.lectures.part1basics
 /**
  * Call by Name vs. Call by Value Takeaways:
  * <ol>
+ *   <li>Scala specific!!!</li>
  *   <li>Call by value:</li>
  *   <ul>
- *     <li>Value is computed before call</li>
+ *     <li>Actual value is computed before call</li>
  *     <li>Same value used everywhere</li>
  *   </ul>
- *   <li>Call by name:</li>
+ *   <li>Call by name (=>):</li>
  *   <ul>
  *     <li>Expression is passed literally</li>
- *     <li>Expression is evaluated at every use within</li>
+ *     <li>Expression is evaluated at every use within the function definition</li>
+ *     <li>Very useful for things that might fail (try type), like lazy streams</li?
  *   </ul>
  * </ol>
  */
@@ -28,12 +30,12 @@ object CBNvsCBV extends App {
     println("Call by name: " + x)
   }
 
-  calledByValue(System.nanoTime())  //these 2 are the same
-  calledByName(System.nanoTime())   //these 2 are different
+  calledByValue(System.nanoTime())  //These 2 are the SAME
+  calledByName(System.nanoTime())   //These 2 are DIFFERENT
 
   def infinite(): Int = 1 + infinite()
   def printFirst(x: Int, y: => Int) = println(x)
 
-  printFirst(infinite(), 34)  //StackOverflow error
-  printFirst(34, infinite())  //Just fine (execution delayed and NEVER used)
+  printFirst(infinite(), 34)        //StackOverflow error!!!
+  printFirst(34, infinite())        //Just fine (infinite execution delayed and NEVER evaluated)
 }
