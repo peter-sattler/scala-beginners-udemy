172bedd2b923f24892fc22ec4b037101d193b1e4
Pete Sattler <peter@sattler22.net>
2022-01-17 21:24:01 -0500

    Updated section 2 - smart operations on strings

--- src/main/scala/net/sattler22/lectures/part1basics/StringOps.scala
+++ src/main/scala/net/sattler22/lectures/part1basics/StringOps.scala
@@ -18,9 +18,10 @@ object StringOps extends App {
 
   //Java string functions:
   val s1 = "Hello, I am learning Scala"
-  println(s1.charAt(2))        //An "l"
-  println(s1.substring(7,11))  //I am
+  println(s1.charAt(2))               //The letter "l"
+  println(s1.substring(7,11))         //"I am"
   println(s1.split(" ").toList)
+  println(s1.startsWith("Hello"))
   println(s1.replace(" ", "-"))
   println(s1.toLowerCase())
   println(s1.length)
@@ -28,25 +29,22 @@ object StringOps extends App {
   //Scala-specific string functions:
   val numString1 = "45"
   val num1 = numString1.toInt
-  println('a' +: numString1 :+ 'z')  //Prepending and appending respect
+  println('a' +: numString1 :+ 'z')   //Prepending and appending respectively
   println(s1.reverse)
-  println(s1.take(2))  //He
+  println(s1.take(2))                 //"He"
 
-  //Scala-specific string interpolation:
+  //Scala-specific S-interpolation:
   val name = "Pete"
   val age = 56
-  val greeting1 = s"Hello, my name is $name and I am $age years old"
-  println(greeting1)
-  val greeting2 = s"Hello, my name is $name and I am turning ${age + 1} years old"
-  println(greeting2)
+  println(s"Hello, my name is $name and I am $age years old")
+  println(s"Hello, my name is $name and I am turning ${age + 1} years old")
 
-  //Scala-specific F-interpolation (printf formats):
+  //Scala-specific F-interpolation (supports printf formatting):
   val speed = 1.2f
-  val myth = f"$name can eat $speed%2.2f burgers per minute"
-  println(myth)
+  println(f"$name%s can eat $speed%2.2f burgers per minute")
 
   //Scala-specific Raw-interpolation:
   println(raw"This is a \n newline")  //Ignores escaped characters
   val escaped = "This is a \n newline"
-  println(raw"$escaped")
+  println(raw"$escaped")              //Otherwise, injected variables DO get escaped
 }
