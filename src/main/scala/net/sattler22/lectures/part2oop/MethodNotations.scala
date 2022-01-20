b5c3044f1f1068e35ddf64cfddafc74693850eac
Pete Sattler <peter@sattler22.net>
2022-01-19 23:23:58 -0500

    Updated section 3 - method notations

--- src/main/scala/net/sattler22/lectures/part2oop/MethodNotations.scala
+++ src/main/scala/net/sattler22/lectures/part2oop/MethodNotations.scala
@@ -7,65 +7,63 @@ import scala.language.postfixOps
  * <ol>
  *   <li>Gets Scala closer to natural language</li>
  *   <li>All operators are methods</li>
- *   <li>Syntactic Sugar!!!</li>
- *   <li>Infix notation (single parameter only) - use object method parameter in a more english like fashion</li>
- *   <li>Prefix notation (+ - ! ~) - use with unary operators</li>
- *   <li>Postfix notation (parameter-less only) - Do not need dot</li>
- *   <li>The apply method (single parameter only)- allows objects to be called as functions</li>
+ *   <li>Syntactic Sugar:</li>
+ *   <ul>
+ *     <li>Infix (or operator) notation (single parameter only) uses an object method parameter in a more english-like fashion</li>
+ *     <li>Prefix notation (+ - ! ~) is used with unary operators (unary_ prefix)</li>
+ *     <li>Postfix notation (parameter-less only) can be used to avoid the dot operator</li>
+ *   </ul>
+ *   <li>The apply method (single parameter only) allows objects to be called as functions</li>
  * </ol>
  */
 object MethodNotations extends App {
 
-  /** Lesson */
-
-  //SYNTACTIC SUGAR - Infix (or operator) notation:
+  //Infix (or operator) notation (syntactic sugar):
   //NOTE: For single parameter methods only!!!
   val mary = new Person("Mary", "Inception")
   println(mary.likes("Inception"))
   println(mary likes "Inception")
 
-  //"Operators" in Scala
+  //"Operators" are methods in Scala:
   //Method names are very flexible (+ is allowed, etc.)
   val tom = new Person("Tom", "Fight Club")
-  println(mary + tom)   //Mary is hanging out with Tom
-  println(mary.+(tom))  //Same thing!!!
-  println(1 + 2)
-  println(1.+(2))       //Mathematical operations are methods too!!!
+  println(mary + tom)        //Mary is hanging out with Tom
+  println(mary.+(tom))       //Mary dot plus (a method) with parameter tom
+  println(1 + 2)             //All operators are methods and work the same way!!!
+  println(1.+(2))            //One dot plus (an operator) with parameter two
 
-  //SYNTACTIC SUGAR - Prefix notation:
-  //NOTE: Works only with - + ~ and !
-  val x = - 1
-  val y = 1.unary_-  //Same as above
+  //Prefix notation (syntactic sugar):
+  //NOTE: Only works with the - + ~ and ! operators
+  val x = -1
+  val y = 1.unary_-          //Unary operators are methods with a "unary_" prefix
   println(mary.unary_!)
-  println(!mary)     //Cool ;)
+  println(!mary)             //Cool ;)
 
-  //SYNTACTIC SUGAR - Postfix notation:
+  //Postfix notation (syntactic sugar):
   //NOTE: Parameter-less methods only!!!
   //NOTE: Needs import above!!!
   println(mary.isAlive)
-  println(mary isAlive)
+  println(mary isAlive)      //Rarely used in practice (introduces ambiguities reading the code)
 
-  //SYNTACTIC SUGAR - Apply method name:
-  // !!!!! SUPER IMPORTANT !!!!!
-  println(mary.apply())  //Can call mary as a function
-  println(mary())        //Works too!!!
+  //Apply method name:
+  //!!!!! SUPER IMPORTANT !!!!!
+  println(mary.apply())      //"Hi, my name is Mary and I like Inception"
+  println(mary())            //Equivalent!!!
 
   /** Exercises */
-
-  /*
-   * 1. Overload the + operator (infix operator)
-   *   -> mary + "the rock star" => new Person "Mary (the rock star)"
-   * 2. Add an age to Person class
-   *   -> Add a unary + operator to increment the age and return a new person
-   * 3. Add a "learns" method in the Person class => Mary learns Scala
-   *   -> Add a learnsScala method, calls leans method with "Scala"
-   *   -> Use it in postfix notation
-   * 4. Overload the apply method
-   *   -> mary.apply(2) => Mary watched Inception 2 times
-   */
+  //For the Person class:
+  // 1. Overload the + operator (infix operator):
+  //   -> mary + "the rock star" returns a new person the name "Mary (the rock star)"
+  // 2. Add an age:
+  //   -> Add a unary + operator to increment the age and return a new, older person
+  // 3. Add a "learns" method to return "Mary learns Scala":
+  //   -> Then add a learnsScala method which calls leans method with "Scala"
+  //   -> Use it in postfix notation
+  // 4. Overload the apply method:
+  //   -> mary.apply(2) returns "Mary watched Inception 2 times"
   val maryRockStar = mary + "the rock star"
-  println(maryRockStar())    //Hi, my name is Mary (the rock star) and I like Inception (calls apply)
-  println((+mary).age)       //Unary operator - An older Mary
+  println(maryRockStar())    //"Hi, my name is Mary (the rock star) and I like Inception" (calls apply method)
+  println((+mary).age)       //Unary operator... An older Mary with an age of one
   println(mary learns "Scala")
   println(mary learnsScala)  //Postfix notation
   println(mary(2))           //Calls apply method
@@ -75,7 +73,7 @@ object MethodNotations extends App {
     def +(person: Person) = s"${this.name} is hanging out with ${person.name}"
     def +(nickName: String) = new Person(s"$name ($nickName)", favoriteMovie)
     //NOTE: Space after method name is needed here!!!
-    def unary_! = s"$name, what the heck?!"
+    def unary_! = s"$name, what the heck?!?!"
     def unary_+ = new Person(name, favoriteMovie, age + 1)
     def isAlive = true
     def learns(topic: String) = s"$name is learning $topic"
