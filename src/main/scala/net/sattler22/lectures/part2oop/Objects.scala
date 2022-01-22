e47381a4e53050a7d9e700ce08a6cde436c373a1
Pete Sattler <peter@sattler22.net>
2022-01-21 20:19:14 -0500

    Updated section 3 - scala objects

--- src/main/scala/net/sattler22/lectures/part2oop/Objects.scala
+++ src/main/scala/net/sattler22/lectures/part2oop/Objects.scala
@@ -3,33 +3,35 @@ package net.sattler22.lectures.part2oop
 /**
  * Scala Objects Takeaways:
  * <ol>
- *   <li>Scala doesn't have "static" values/methods</li>
- *   <li>Scala uses object instead:</li>
+ *   <li>Scala does NOT have class-level (static) functionality (values/methods)</li>
+ *   <li>Scala uses <i>object</i> instead:</li>
  *   <ul>
- *     <li>Are in their own class/li>
- *     <li>Are the only instance</li>
- *     <li>Singleton pattern in one line!</li>
+ *     <li>They are in their own class</li>
+ *     <li>It is the only instance</li>
+ *     <li>The singleton pattern in one line!!!</li>
  *     <li>They can extend a class</li>
+ *     <li>They can NOT receive parameters</li>
  *   </ul>
- *   <li>Scala Companions:/li>
+ *   <li>Scala Companions:</li>
  *   <ul>
- *     <li>Write both an object and class using the same name and having the same scope<</li>
- *     <li>object - class-level functionality</li>
- *     <li>class - instance-level functionality</li>
- *     <li>Can access each other's private members</li>
- *     <li>Scala is more OO than Java!</li>
+ *     <li>A design pattern where both the class-level and instance-level functionality use the same name and have the same scope<</li>
+ *     <li>The keyword <i>object</i> is used for class-level functionality</li>
+ *     <li>The keyword <i>class</i> is used for instance-level functionality</li>
+ *     <li>They can access each other's private members</li>
+ *     <li>Scala is more object-oriented than Java!!!</li>
  *   </ul>
  *   <li>Scala Application:</li>
  *   <ul>
  *     <li>It is a Scala object</li>
- *     <li>Must have method def main(args: Array[String]): Unit</li>
- *     <li>OR it can extend App</li>
+ *     <li>It MUST have method: <i>def main(args: Array[String]): Unit</i></li>
+ *     <li>OR it can <i>extend App</i></li>
  *   </ul>
  * </ol>
  */
 object Objects {
 
   object Person {
+    //Class-level functionality:
     val N_EYES = 2;
     def canFly: Boolean = true
     //Factory method:
@@ -37,7 +39,7 @@ object Objects {
   }
 
   class Person(val name: String) {
-    //instance level functionality
+    //Instance-level functionality...
   }
 
   //INSTEAD OF: extends App
@@ -47,10 +49,10 @@ object Objects {
 
     val person1 = Person
     val person2 = Person
-    println (person1 == person2) //Point to same singleton instance
+    println (person1 == person2)    //Point to same singleton instance
 
     val mary = new Person ("Mary")
     val john = new Person ("John")
-    val bobby = Person (mary, john) //Calls apply method
+    val bobby = Person(mary, john)  //Calls apply method
   }
 }
