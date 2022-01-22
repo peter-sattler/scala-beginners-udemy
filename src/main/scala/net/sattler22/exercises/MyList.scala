4e50de055c53111248f47fba2e684266ece6a298
Pete Sattler <peter@sattler22.net>
2022-01-22 12:08:34 -0500

    Updated section 3 - singly linked list exercise

--- src/main/scala/net/sattler22/exercises/MyList.scala
+++ src/main/scala/net/sattler22/exercises/MyList.scala
@@ -6,14 +6,14 @@ package net.sattler22.exercises
  * <h3>Initial Requirements</h3>
  * <ol>
  *   <li>Describes an immutable list of any type</li>
- *   <li>Singly linked list</li>
+ *   <li>A singly linked list</li>
  *   <li>Methods:</li>
  *   <ul>
- *     <li>head = first element of the list</li>
- *     <li>tail = remainder of the list</li>
- *     <li>isEmpty = is this list empty</li>
- *     <li>add(int) => new list with this element added</li>
- *     <li>toString => a string representation of the list</li>
+ *     <li>head returns the first element of the list</li>
+ *     <li>tail returns the remainder of the list</li>
+ *     <li>isEmpty checks if the list does not contain any elements</li>
+ *     <li>add(int) adds an element to the list and returns a new (immutable) list</li>
+ *     <li>toString provides a meaningful string representation of the list</li>
  *   </ul>
  * </ol>
  *
@@ -60,20 +60,20 @@ abstract class MyList[+A] {
   def head: A
   def tail: MyList[A]
   def isEmpty: Boolean
-  def add[B >: A](element: B): MyList[B]      //Use return type [B >: A] to avoid contravariant error message
+  def add[B >: A](element: B): MyList[B]                               //Use return type [B >: A] to avoid contravariant error message
   def map[B](transformer: MyTransformer[A, B]): MyList[B]              //OO Exercises Enhancements
   def filter(predicate: MyPredicate[A]): MyList[A]                     //OO Exercises Enhancements
   def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B]  //OO Exercises Enhancements
-  def ++[B >: A](list: MyList[B]): MyList[B]  //Concatenation
-  def printElements: String  //Polymorphic call - Delegate implementation to sub-class
-  override def toString: String = s"[$printElements]"
+  def ++[B >: A](list: MyList[B]): MyList[B]                           //Concatenation
+  def printElements: String                                            //Polymorphic call which delegates implementation to a sub-class
+  override def toString: String = s"[$printElements]"                  //Override scala.AnyRef class
 }
 
 //Empty List:
-//Use object since its a singleton and only ONE empty instance is needed
-//NOTE: Three question marks will throw a "not implemented" error
-//NOTE: The Nothing type is ALWAYS a proper substitute for AnyType
-//Good use for a case class!!!
+//  -> Use object since its a singleton and only ONE empty instance is needed
+//  -> Three question marks will throw a "not implemented" error
+//  -> The Nothing type is ALWAYS a proper substitute for AnyType
+//  -> Good use for a case class!!!
 case object Empty extends MyList[Nothing] {
   def head: Nothing = throw new NoSuchElementException
   def tail: MyList[Nothing] = throw new NoSuchElementException
@@ -82,13 +82,14 @@ case object Empty extends MyList[Nothing] {
   def map[B](transformer: MyTransformer[Nothing, B]): MyList[B] = Empty
   def filter(predicate: MyPredicate[Nothing]): MyList[Nothing] = Empty
   def flatMap[B](transformer: MyTransformer[Nothing, MyList[B]]): MyList[B] = Empty
-  def ++[B >: Nothing](list: MyList[B]): MyList[B] = list  //List remains unchanged!!!
+  def ++[B >: Nothing](list: MyList[B]): MyList[B] = list              //List remains unchanged!!!
   def printElements: String = ""
 }
 
 //Non-Empty list:
-//Must be covariant too!!!
-//Good use for a case class!!!
+//  -> Composed of an element (head) and the rest of the list (tail)
+//  -> Must be covariant too!!!
+//  -> Good use for a case class!!!
 case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
   def head: A = h
   def tail: MyList[A] = t
@@ -102,13 +103,13 @@ case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
   def filter(predicate: MyPredicate[A]): MyList[A] = {
     //Test head and always filter the tail:
     //Example: filter [1,2,3], results in [2]
-    // -> [1,2,3].filter(element % 2 == 0)            //element=1, 1 / 2 (R=1), test fails (else), remove head, filter=[2,3]
-    // -> [2,3].filter(element % 2 == 0)              //element=2, 2 / 2 (R=0), test passes (if), new Cons with head=2, filter=[3]
-    // -> new Cons(2, [3].filter(element % 2 == 0)    //element=3: 3 / 2 (R=1), test fails (else), remove head, filter=[2]
-    // -> new Cons(2. Empty.filter(element % 2 == 0)  //element=2: 2 / 2 (R=0), test passes (if), new Cons with head=2, filter=[Empty]
-    // -> new Cons(2, Empty)                          //result=[2]
-    if (predicate.test(h)) new Cons(h, t.filter(predicate)) //head included
-    else t.filter(predicate)                                //head filtered out (excluded)
+    // -> [1,2,3].filter(element % 2 == 0)                  //element=1, 1 / 2 (R=1), test fails (else), remove head, filter=[2,3]
+    // -> [2,3].filter(element % 2 == 0)                    //element=2, 2 / 2 (R=0), test passes (if), new Cons with head=2, filter=[3]
+    // -> new Cons(2, [3].filter(element % 2 == 0)          //element=3: 3 / 2 (R=1), test fails (else), remove head, filter=[2]
+    // -> new Cons(2. Empty.filter(element % 2 == 0)        //element=2: 2 / 2 (R=0), test passes (if), new Cons with head=2, filter=[Empty]
+    // -> new Cons(2, Empty)                                //result=[2]
+    if (predicate.test(h)) new Cons(h, t.filter(predicate)) //Head included
+    else t.filter(predicate)                                //Head filtered out (excluded)
   }
   def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B] = {
     //Example: [1, 2].flatMap(n => n + 1) results in [1,2, 2,3]
@@ -128,7 +129,8 @@ case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
   }
   def printElements: String = {
     if (t.isEmpty) "" + h
-    else h + " " + t.printElements  //Scala 2 only!!!
+    //else h + " " + t.printElements              //This only works in Scala 2
+    else s"$h ${t.printElements}"                 //For Scala 3, use a String interpolator
   }
 }
 
@@ -145,14 +147,14 @@ trait MyTransformer[-A, B] {
 object ListTest extends App {
   //Initial Requirements:
   val listOfIntegers: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
-  println(listOfIntegers.head)         //1
-  println(listOfIntegers.tail.head)    //2
-  println(listOfIntegers.add(4).head)  //4 - Element gets added to head
-  println(listOfIntegers.isEmpty)      //false
-  println(listOfIntegers.toString)     //[1 2 3]
+  println(listOfIntegers.head)                    //1
+  println(listOfIntegers.tail.head)               //2
+  println(listOfIntegers.add(4).head)             //4 - Element gets added to head
+  println(listOfIntegers.isEmpty)                 //false
+  println(listOfIntegers.toString)                //[1 2 3]
 
   val listOfStrings: MyList[String] = new Cons("Hello", new Cons("Scala", Empty))
-  println(listOfStrings.toString)      //Hello Scala
+  println(listOfStrings.toString)                 //Hello Scala
 
   //Empty should be a proper value for a list of any type:
   val listOfIntegersEmpty: MyList[Int] = Empty
@@ -174,7 +176,7 @@ object ListTest extends App {
   //OO Exercise Example 3:
   //[1, 2, 3].flatMap(n => n + 1) ==> [1,2, 2,3, 3,4]
   val testConcatenation: MyList[Int] = new Cons(4, new Cons(5, Empty))
-  println(listOfIntegers ++ testConcatenation)  //[1,2,3,4,5]
+  println(listOfIntegers ++ testConcatenation)    //[1,2,3,4,5]
   println(listOfIntegers.flatMap(new MyTransformer[Int, MyList[Int]] {
     override def transform(element: Int): MyList[Int] = new Cons(element, new Cons(element + 1, Empty))
   }))
