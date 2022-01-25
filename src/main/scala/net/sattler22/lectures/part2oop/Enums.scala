61098191b11f0959a7e0c9218879a19f7ec04e1f
Pete Sattler <peter@sattler22.net>
2022-01-24 19:05:44 -0500

    Updated section 3 - enums (scala 3 only)

--- src/main/scala/net/sattler22/lectures/part2oop/Enums.scala
+++ src/main/scala/net/sattler22/lectures/part2oop/Enums.scala
@@ -3,11 +3,10 @@ package net.sattler22.lectures.part2oop
 /**
  * Enums Takeaways:
  * <ol>
- *   <li>Enums are only supported in Scala 3!!!</li>
- *   <li>A full fledged data type (methods allowed, etc.)</li>
+ *   <li>A full fledged data type (allows fields, methods, etc.)</li>
  *   <li>Constructor arguments are supported, though the syntax is a bit verbose</li>
  *   <li>Companion objects are supported too</li>
- *   <li>WARNING: Use with caution - code was not actually compiled since it is Scala 3</li>
+ *   <li>Only Scala 3 has first-class support for Enums!!!</li>
  * </ol>
  */
 object Enums extends App {
@@ -25,24 +24,21 @@ object Enums extends App {
 
   //Constructor arguments:
   enum PermissionsWithBits(bits: Int) {
-    case READ extends PermissionsWithBits(4)              //100
-    case WRITE extends PermissionsWithBits(2)             //010
-    case EXECUTE extends PermissionsWithBits(1)           //001
-    case NONE extends PermissionsWithBits(0)              //000
+    case READ extends PermissionsWithBits(4)               //100
+    case WRITE extends PermissionsWithBits(2)              //010
+    case EXECUTE extends PermissionsWithBits(1)            //001
+    case NONE extends PermissionsWithBits(0)               //000
   }
 
-  //Enum companion objects are supported:
+  //Companion objects are supported:
   object PermissionsWithBits {
     def fromBits(bits: Int): PermissionsWithBits = PermissionsWithBits.NONE
   }
 
-  //Ordinal is the zero-based enum index:
-  val perms1Ordinal = perms1.ordinal
-  println(perms1Ordinal)                                   //READ ordinal = 0
-
-  //Array of all possible values of the enum
-  val allPerms = PermissionsWithBits.values()
-
-  //Create an enum instance from a string:
-  val readPerm: Permissions = Permissions.valueOf("READ")  //Permissions.READ
+  //Standard API:
+  val perms1Ordinal = perms1.ordinal                       //Get zero-based enum index
+  val allPerms = PermissionsWithBits.values                //Array of all possible values
+  val readPerm: Permissions = Permissions.valueOf("READ")  //Create an instance from a string
+  println(perms1Ordinal)                                   //0
+  println(readPerm)                                        //Permissions.READ
 }
