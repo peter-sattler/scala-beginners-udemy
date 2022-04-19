3cb482e66f66e9ffa3bbd5af5165ae954a45a7f5
Pete Sattler <peter@sattler22.net>
2022-04-19 00:06:05 -0400

    Added section 4 - higher order functions (hofs) and curries exercises

--- src/main/scala/net/sattler22/exercises/MyList.scala
+++ src/main/scala/net/sattler22/exercises/MyList.scala
@@ -61,6 +61,17 @@ package net.sattler22.exercises
  * <ol>
  *   <li>Replace all <i>FunctionX</i> calls with lambdas</li>
  * </ol>
+ *
+ * <h3>Section 4: Higher Order Functions (HOFs) and Curries Exercise</h3>
+ * <ol>
+ *   <li>New methods:</li>
+ *   <ul>
+ *     <li><i>forEach</i> which receives a function <i>A => Unit</i> and applies it to every element in the list</li>
+ *     <li><i>sort</i> which receives a comparator <i>(A, A) => Int</i> (-1 if less than, zero if equal and +1 if greater than) and returns another <i>MyList[A]</i></li>
+ *     <li><i>zipWith</i> which receives another list and a zip function <i>(A, A) => B</i> (two elements to a new one) and returns <i>myList[B]</i></li>
+ *     <li><i>fold</i> which receives a start value and a function as separate parameter lists (curried) and returns a value</li>
+ *   </ul>
+ * </ol>
  */
 abstract class MyList[+A] {
   def head: A
@@ -70,6 +81,10 @@ abstract class MyList[+A] {
   def map[B](transformer: A => B): MyList[B]                           //Higher-order functions exercise
   def filter(predicate: A => Boolean): MyList[A]                       //Higher-order functions exercise
   def flatMap[B](transformer: A => MyList[B]): MyList[B]               //Higher-order functions exercise
+  def forEach(fx: A => Unit): Unit                                     //HOFs and curries exercise
+  def sort(comparator: (A, A) => Int): MyList[A]                       //HOFs and curries exercise
+  def zipWith[B, C](list: MyList[B], zip: (A, B) => C): MyList[C]      //HOFs and curries exercise
+  def fold[B](start: B)(operator: (B, A) => B): B                      //HOFs and curries exercise
   def ++[B >: A](list: MyList[B]): MyList[B]                           //Concatenation
   def printElements: String                                            //Polymorphic call which delegates implementation to a sub-class
   override def toString: String = s"[$printElements]"                  //Override scala.AnyRef class
@@ -84,10 +99,17 @@ case object Empty extends MyList[Nothing] {
   def head: Nothing = throw new NoSuchElementException
   def tail: MyList[Nothing] = throw new NoSuchElementException
   def isEmpty: Boolean = true
-  def add[B >: Nothing](element: B): MyList[B] = new Cons(element, Empty)
+  def add[B >: Nothing](element: B): MyList[B] = Cons(element, Empty)
   def map[B](transformer: Nothing => B): MyList[B] = Empty
   def filter(predicate: Nothing => Boolean): MyList[Nothing] = Empty
   def flatMap[B](transformer: Nothing => MyList[B]): MyList[B] = Empty
+  def forEach(fx: Nothing => Unit): Unit = ()                          //Return unit value
+  def sort(comparator: (Nothing, Nothing) => Int): MyList[Nothing] = Empty
+  def zipWith[B, C](list: MyList[B], zip: (Nothing, B) => C): MyList[C] = {
+    if(!list.isEmpty) throw new RuntimeException("Lists must have the same number of elements")
+    else Empty
+  }
+  def fold[B](start: B)(operator: (B, Nothing) => B): B = start
   def ++[B >: Nothing](list: MyList[B]): MyList[B] = list              //List remains unchanged!!!
   def printElements: String = ""
 }
@@ -100,26 +122,26 @@ case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
   def head: A = h
   def tail: MyList[A] = t
   def isEmpty: Boolean = false
-  def add[B >: A](element: B): MyList[B] = new Cons(element, this)
+  def add[B >: A](element: B): MyList[B] = Cons(element, this)
   def map[B](transformer: A => B): MyList[B] = {
     //Transform the head and tail:
     //NOTE: Result will have the same number of elements!!!
     //Example: [1,2,3].map(n * 2)
-    // -> new Cons(2, [2,3].map(n * 2))
-    // -> new Cons(2, new Cons(4, [3].map(n * 2)))
-    // -> new Cons(2, new Cons(4, new Cons(6, Empty.map(n * 2))))
-    // -> new Cons(2, new Cons(4, new Cons(6, Empty)))
-    new Cons(transformer(h), t.map(transformer))
+    // -> Cons(2, [2,3].map(n * 2))
+    // -> Cons(2, Cons(4, [3].map(n * 2)))
+    // -> Cons(2, Cons(4, Cons(6, Empty.map(n * 2))))
+    // -> Cons(2, Cons(4, Cons(6, Empty)))
+    Cons(transformer(h), t.map(transformer))
   }
   def filter(predicate: A => Boolean): MyList[A] = {
     //Test head and always filter the tail:
     //Example: filter [1,2,3], results in [2]
     // -> [1,2,3].filter(element % 2 == 0)                  //element=1, 1 / 2 (R=1), test fails (else), remove head, filter=[2,3]
-    // -> [2,3].filter(element % 2 == 0)                    //element=2, 2 / 2 (R=0), test passes (if), new Cons with head=2, filter=[3]
-    // -> new Cons(2, [3].filter(element % 2 == 0)          //element=3: 3 / 2 (R=1), test fails (else), remove head, filter=[2]
-    // -> new Cons(2. Empty.filter(element % 2 == 0)        //element=2: 2 / 2 (R=0), test passes (if), new Cons with head=2, filter=[Empty]
-    // -> new Cons(2, Empty)                                //result=[2]
-    if (predicate(h)) new Cons(h, t.filter(predicate))      //Head included
+    // -> [2,3].filter(element % 2 == 0)                    //element=2, 2 / 2 (R=0), test passes (if), Cons with head=2, filter=[3]
+    // -> Cons(2, [3].filter(element % 2 == 0)              //element=3: 3 / 2 (R=1), test fails (else), remove head, filter=[2]
+    // -> Cons(2. Empty.filter(element % 2 == 0)            //element=2: 2 / 2 (R=0), test passes (if), Cons with head=2, filter=[Empty]
+    // -> Cons(2, Empty)                                    //result=[2]
+    if (predicate(h)) Cons(h, t.filter(predicate))          //Head included
     else t.filter(predicate)                                //Head filtered out (excluded)
   }
   def flatMap[B](transformer: A => MyList[B]): MyList[B] = {
@@ -130,32 +152,59 @@ case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
     // -> [1,2,2,3]
     transformer(h) ++ t.flatMap(transformer)
   }
+  def forEach(fx: A => Unit): Unit = {
+    fx(h)
+    tail.forEach(fx)
+  }
+  def sort(comparator: (A, A) => Int): MyList[A] = {
+    def insert(x: A, sortedList: MyList[A]): MyList[A] = {                   //Insertion sort
+      if (sortedList.isEmpty) Cons(x, Empty)                                 //Return only one element
+      else if (comparator(x, sortedList.head) <= 0) Cons(x, sortedList)      //New smallest element is x
+      else Cons(sortedList.head, insert(x, sortedList.tail))                 //New smallest element is the head
+    }
+    val sortedTail = tail.sort(comparator)
+    insert(head, sortedTail)
+  }
+  def zipWith[B, C](list: MyList[B], zip: (A, B) => C): MyList[C] = {
+    //Example: [4,5].zipWith(["Hello","Scala", _ + "-" + _)
+    // -> ["4-Hello", "5-Scala"]
+    if(list.isEmpty) throw new RuntimeException("Lists must have the same number of elements")
+    else Cons(zip(head, list.head), tail.zipWith(list.tail, zip))
+  }
+  //Example: [1,2,3].fold(0)(+)
+  // -> [2,3].fold(1)(+)
+  // -> [3].fold(3)(+)
+  // -> [].fold(6)(+)
+  // -> 6
+  def fold[B](start: B)(operator: (B, A) => B): B = {
+    tail.fold(operator(start, head))(operator)              //Compose start value with head, then do the rest
+  }
   def ++[B >: A](list: MyList[B]): MyList[B] = {
     //Example: [1,2] ++ [3,4,5], results in [1,2,3,4,5]
-    // -> new Cons(1, [2] ++ [3,4,5])
-    // -> new Cons(1, new Cons(2, [Empty] ++ [3,4,5]))
-    // -> new Cons(1, new Cons(2, [3,4,5])) which is just...
-    // -> new Cons(1, new Cons(2, new Const(3, new Const(4, new Const(5)))))
-    new Cons(h, t ++ list)
+    // -> Cons(1, [2] ++ [3,4,5])
+    // -> Cons(1, Cons(2, [Empty] ++ [3,4,5]))
+    // -> Cons(1, Cons(2, [3,4,5])) which is just...
+    // -> Cons(1, Cons(2, Const(3, Const(4, Const(5)))))
+    Cons(h, t ++ list)
   }
   def printElements: String = {
     if (t.isEmpty) "" + h
-    //else h + " " + t.printElements              //This only works in Scala 2
-    else s"$h ${t.printElements}"                 //For Scala 3, use a String interpolator
+    //else h + " " + t.printElements                        //This only works in Scala 2
+    else s"$h ${t.printElements}"                           //For Scala 3, use a String interpolator
   }
 }
 
 //Scala application:
 object ListTest extends App {
   //Initial Requirements:
-  val listOfIntegers: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
+  val listOfIntegers: MyList[Int] = Cons(1, Cons(2, Cons(3, Empty)))
   println(listOfIntegers.head)                    //1
   println(listOfIntegers.tail.head)               //2
   println(listOfIntegers.add(4).head)             //4 - Element gets added to head
   println(listOfIntegers.isEmpty)                 //false
   println(listOfIntegers.toString)                //[1 2 3]
 
-  val listOfStrings: MyList[String] = new Cons("Hello", new Cons("Scala", Empty))
+  val listOfStrings: MyList[String] = Cons("Hello", Cons("Scala", Empty))
   println(listOfStrings.toString)                 //"Hello Scala"
 
   //Empty should be a proper value for a list of Any type:
@@ -163,7 +212,7 @@ object ListTest extends App {
   val listOfStringsEmpty: MyList[String] = Empty
 
   //Concatenation operator:
-  val testConcatenation: MyList[Int] = new Cons(4, new Cons(5, Empty))
+  val testConcatenation: MyList[Int] = Cons(4, Cons(5, Empty))
   println(listOfIntegers ++ testConcatenation)    //[1,2,3,4,5]
 
   //OO Exercise Example 1:
@@ -190,14 +239,21 @@ object ListTest extends App {
   //OO Exercise Example 3:
   //[1, 2, 3].flatMap(n => n + 1) ==> [1,2, 2,3, 3,4]
   //println(listOfIntegers.flatMap(new Function1[Int, MyList[Int]] {
-  //  override def apply(element: Int): MyList[Int] = new Cons(element, new Cons(element + 1, Empty))
+  //  override def apply(element: Int): MyList[Int] = Cons(element, Cons(element + 1, Empty))
   //}))
 
   //Lambda Replacement Exercise Example 3:
   //[1, 2, 3].flatMap(n => n + 1) ==> [1,2, 2,3, 3,4]
-  println(listOfIntegers.flatMap(element => new Cons[Int](element, new Cons[Int](element + 1, Empty))))
+  println(listOfIntegers.flatMap(element => Cons[Int](element, Cons[Int](element + 1, Empty))))
 
   //Case Classes Enhancement:
-  val listOfIntegersClone: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
+  val listOfIntegersClone: MyList[Int] = Cons(1, Cons(2, Cons(3, Empty)))
   println(listOfIntegers == listOfIntegersClone)  //Case class equals() returns true
+
+  //HOFs and Curries Exercise:
+  listOfIntegers.forEach(x => println(x))         //Lambda
+  listOfIntegers.forEach(println)                 //Short-hand notation
+  println(listOfIntegers.sort((x, y) => y - x))   //Reverse the elements
+  println(testConcatenation.zipWith[String, String](listOfStrings, _ + "-" + _))
+  println(listOfIntegers.fold(0)(_ + _))          //Often called reduce
 }
--- src/main/scala/net/sattler22/lectures/part3fp/HOFsCurries.scala
+++ src/main/scala/net/sattler22/lectures/part3fp/HOFsCurries.scala
@@ -31,7 +31,7 @@ object HOFsCurries extends App {
   //  -> Second: String, (Int => Boolean)) => Int (another function which returns an integer)
   //Return Type (RHS):
   //  -> Int => Int (a function that takes an integer and returns an integer)
-  val superFunction: (Int, (String, (Int => Boolean)) => Int) => (Int => Int) = null
+  val superFunction: (Int, (String, Int => Boolean) => Int) => Int => Int = null
 
   //Define a function that applies a function n times over a given value x:
   //nTimes(f, n, x) where:
@@ -42,7 +42,7 @@ object HOFsCurries extends App {
   //nTimes(f, n, x) would be generalized as f(f(...f(x))) or nTimes(f, n-1, f(x))
   @tailrec
   def nTimes(f: Int => Int, n: Int, x: Int): Int = {
-    if (n <= 0) x                   //Nothing to apply so just return the value
+    if (n <= 0) x                                         //Nothing to apply so just return the value
     else nTimes(f, n-1, f(x))
   }
   val plusOne = (x: Int) => x + 1
@@ -52,8 +52,8 @@ object HOFsCurries extends App {
   //nTimesBetter(f, n) = x => f(f(...f(x))) which returns a lambda which can be applied later to various values
   //increment10 = nTimesBetter(plusOne, 10) = x => plusOne(plusOne(...plusOne...(x)))
   //val y = increment10(1)
-  def nTimesBetter(f: Int => Int, n: Int): (Int => Int) = {
-    if (n <= 0) (x: Int) => x       //Nothing to apply so return the identity function
+  def nTimesBetter(f: Int => Int, n: Int): Int => Int = {
+    if (n <= 0) (x: Int) => x                             //Nothing to apply so return the identity function
     else (x: Int) => nTimesBetter(f, n-1)(f(x))
   }
   val plusTen = nTimesBetter(plusOne, 10)
@@ -61,8 +61,8 @@ object HOFsCurries extends App {
 
   //Curried functions:
   val superAdder1 = (x: Int) => (y: Int) => x + y
-  def superAdder2: Int => (Int => Int) = (x: Int) => (y: Int) => x + y
-  val adder3 = superAdder2(3)       //Helper function (lambda): y => 3 + y
+  val superAdder2: Int => Int => Int = (x: Int) => (y: Int) => x + y
+  val adder3 = superAdder2(3)                             //Helper function (lambda): y => 3 + y
   println(s"Adder3 returns: [${adder3(10)}]")
   println(s"Super Adder2 returns: [${superAdder2(3)(10)}]")
 
@@ -70,8 +70,47 @@ object HOFsCurries extends App {
   //  -> There can be as many parameter lists as needed, but their type MUST be specified!!!
   //  -> Smaller functions can then be created for later use
   def curriedFormatter(c: String)(x: Double): String = c.format(x)
-  val standardFormatter: (Double => String) = curriedFormatter("%4.2f")
-  val preciseFormatter: (Double => String) = curriedFormatter("%10.8f")
+  val standardFormatter: Double => String = curriedFormatter("%4.2f")
+  val preciseFormatter: Double => String = curriedFormatter("%10.8f")
   println(s"PI using standard formatter: [${standardFormatter(Math.PI)}]")
   println(s"PI using precise formatter: [${preciseFormatter(Math.PI)}]")
+
+  /** Exercises */
+  //Expand MyList with these new methods:
+  //  -> def forEach(fx: A => Unit): Unit
+  //  -> def sort(comparator: (A, A) => Int): MyList[A]
+  //  -> def zipWith[B, C](list: MyList[B], zip: (A, B) => C): MyList[C]
+  //  -> def fold[B](start: B)(operator: (B, A) => B): B
+
+  //Receives a function (that takes two arguments and returns a value) and returns the curried version:
+  def toCurry(fx: (Int, Int) => Int): Int => Int => Int = {
+    x => y => fx(x, y)                                    //Returns a lambda taking a value x which returns a lambda taking a value y
+  }
+  val superAdder3: Int => Int => Int = toCurry(_ + _)
+  val adder4 = superAdder3(4)
+  println(s"Adder4 (curried) returns: [${adder4(17)}]")
+
+  //Receives a curried function and returns the uncurried one:
+  def fromCurry(fx: Int => Int => Int): (Int, Int) => Int = {
+    (x, y) => fx(x)(y)                                    //Returns a lambda that takes two arguments
+  }
+  val simpleAdder = fromCurry(superAdder1)
+  println(s"SimpleAdder (uncurried) returns: [${simpleAdder(4,17)}]")
+
+  //Receives two functions and applies the second one and then the first one:
+  //NOTE: Returns a lambda that applies function gx to x and then function fx
+  def compose[A,B,T](fx: A => B, gx: T => A): T => B = {
+    x => fx(gx(x))                                        //Returns a lambda that applies gx and then fx
+  }
+  val add2 = (x: Int) => x + 2
+  val times3 = (x: Int) => x * 3
+  val composed = compose[Int, Int, Int](add2, times3)
+  println(s"Composed returns: [${composed(4)}]")          //(4 * 3) + 2 = 14
+
+  //Receives two functions and applies the first one and then the other:
+  def andThen[A,B,C](fx: A => B, gx: B => C): A => C = {
+    x => gx(fx(x))                                        //Returns a lambda that applies fx and then gx
+  }
+  val ordered = andThen[Int, Int, Int](add2, times3)
+  println(s"Ordered (andThen) returns: [${ordered(4)}]")  //(4 + 2) * 3 = 18
 }
